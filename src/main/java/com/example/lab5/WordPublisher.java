f663bd6c3d59c6707a546e1e8a1ec74ad1de6263
63070059 <73683000+63070059@users.noreply.github.com>
2022-09-12 11:58:15 +0700

    Initial commit

---
+++ src/main/java/com/example/lab5/WordPublisher.java
@@ -0,0 +1,75 @@
+package com.example.lab5;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class WordPublisher {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    protected Word words = new Word();
+    @RequestMapping(value = "/addBad/{word}", method = RequestMethod.GET)
+    public ArrayList<String> addBadWord(@PathVariable("word") String s){
+        words.badWords.add(s);
+        return words.badWords;
+    }
+    @RequestMapping(value = "/delBad/{word}", method = RequestMethod.GET)
+    public ArrayList<String> deleteBadWord(@PathVariable("word") String s){
+        for(int i = 0; i < words.badWords.size(); i++) {
+            if(words.badWords.get(i).equals(s)){
+                words.badWords.remove(i);
+                break;
+            }
+        }
+        return words.badWords;
+    }
+    @RequestMapping(value = "/addGood/{word}", method = RequestMethod.GET)
+    public ArrayList<String> addGoodWord(@PathVariable("word") String s){
+        words.goodWords.add(s);
+        return words.goodWords;
+    }
+    @RequestMapping(value = "/delGood/{word}", method = RequestMethod.GET)
+    public ArrayList<String> deleteGoodWord(@PathVariable("word") String s){
+        for(int i = 0; i < words.goodWords.size(); i++) {
+            if(words.goodWords.get(i).equals(s)){
+                words.goodWords.remove(i);
+                break;
+            }
+        }
+        return words.goodWords;
+    }
+    @RequestMapping(value = "/proof/{sentence}", method = RequestMethod.GET)
+    public void proofSentence(@PathVariable("sentence") String s){
+        boolean good = false;
+        boolean bad = false;
+        for(int i = 0; i < words.goodWords.size(); i++) {
+            if(s.contains(words.goodWords.get(i))){
+                good = true;
+            }
+        }
+        for(int i = 0; i < words.badWords.size(); i++) {
+            if(s.contains(words.badWords.get(i))){
+                bad = true;
+            }
+        }
+        if(bad && good){
+            System.out.println("Have Both");
+            rabbitTemplate.convertAndSend("Fanout", "", s);
+        }
+        else if(good){
+            System.out.println("Good");
+            rabbitTemplate.convertAndSend("Direct", "good", s);
+        }
+        else if(bad){
+            System.out.println("Bad");
+            rabbitTemplate.convertAndSend("Direct", "bad", s);
+        }
+    }
+}
